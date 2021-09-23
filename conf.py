0a6dff95fd156d8918b810e1d1efe630c9e95674
Asadullah <asadullah.khalid@t-systems.com>
2021-09-23 08:45:43 -0700

    update conf.py for readthedocs theme

--- conf.py
+++ conf.py
@@ -14,6 +14,8 @@
 #
 # import os
 # import sys
+import sphinx_rtd_theme
+
 # sys.path.insert(0, os.path.abspath('.'))
 
 
@@ -75,12 +77,16 @@ pygments_style = None
 # a list of builtin themes.
 #
 html_theme = 'alabaster'
+html_theme = 'sphinx_rtd_theme'
+
 
 # Theme options are theme-specific and customize the look and feel of a theme
 # further.  For a list of options available for each theme, see the
 # documentation.
 #
 # html_theme_options = {}
+html_theme_path = [sphinx_rtd_theme.get_html_theme_path()]
+
 
 # Add any paths that contain custom static files (such as style sheets) here,
 # relative to this directory. They are copied after the builtin static files,
